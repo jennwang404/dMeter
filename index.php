2791214ee464255a1a2ef33f56a6a6c35010eae1
Fish Milnikiewicz <sam464@cornell.edu>
2017-07-18 14:10:12 -0400

    Fixed some url redirect bugs; made dMeter redirect to index

--- index.php
+++ index.php
@@ -140,7 +140,11 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
             $_SESSION['user'] = $login_email;
             $login_message_succ = "Login successful";
             $url = $_SERVER['REQUEST_URI'];
-            $newurl =  substr($url, 0, -9)."account.php";
+            if (substr($url, -9) == "index.php") {
+              $newurl =  substr($url, 0, -9)."account.php";
+            } else {
+              $newurl = $url."account.php";
+            }
             echo '<script type="text/javascript">window.location ='."'".$newurl."'".'</script>';
           } else {
             $login_message =  "Incorrect password";
@@ -171,7 +175,7 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
       </div>
      </div>
     </div>
-    <img class="colelem" id="u986-4" alt="d.Meter" src="images/u986-4.png?crc=416918895" data-image-width="97"/><!-- rasterized frame -->
+    <a href="index.php"><img class="colelem" id="u986-4" alt="d.Meter" src="images/u986-4.png?crc=416918895" data-image-width="97" /></a><!-- rasterized frame -->
     <div class="clearfix colelem" id="u937-10"><!-- content -->
      <p id="u937-2">TRANSPARENT. CONVENIENT. FLEXIBLE.</p>
      <p id="u937-3">&nbsp;</p>
