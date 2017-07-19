1b89db4ef17a37ff94bfac5779fce920499a0690
ntc27 <ntc27@cornell.edu>
2017-07-19 11:19:19 -0400

    changed southside and weekdata

--- weekdata.php
+++ weekdata.php
@@ -1,19 +1,19 @@
 <?php
-    $username = ""; 
-    $password = "";   
-    $host = "localhost";
+    $username = "dmeterco"; 
+    $password = "301CollegeAve#";   
+    $host = "https://p3plcpnl0992.prod.phx3.secureserver.net:2083";
     $database="dMeter";
     $secondGraph = "temperature";
 	if (isset($_GET["secondGraph"])){
 		$secondGraph = $_GET["secondGraph"];
 	}
 	
-	$month = 3;
-	$year = 2017;
+	$month = 7;
+	$year = 2016;
 	
     $conn = new mysqli($host, $username, $password, $database);
 	
-	$sql = "SELECT SUM(EnergyUse) AS EnergyUse, AVG(temp) AS Temp, DAY(t.day) AS Day, MONTH(t.day) AS Month, YEAR(t.day) AS Year FROM energy_usage e INNER JOIN $secondGraph t on DAY(t.day) = DAY(e.day) AND MONTH(t.day) = MONTH(e.day) AND YEAR(t.day) = YEAR(e.day)+9 WHERE MONTH(t.day) = $month AND YEAR(t.day) = $year GROUP BY t.day";
+	$sql = "SELECT Energy, Temperature, DAY(t.day) AS Day, MONTH(t.day) AS Month, YEAR(t.day) AS Year FROM Energy_Uses e INNER JOIN $secondGraph t on t.Day = e.Day WHERE MONTH(t.day) = $month AND YEAR(t.day) = $year GROUP BY t.day";
 	$result = $conn->query($sql);
 	
 	if ($result->num_rows > 0) {
