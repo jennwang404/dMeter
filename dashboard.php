46a4a4ee6ab04ae1da8577c0c417e7071f5bd92e
Fish Milnikiewicz <sam464@cornell.edu>
2017-07-26 15:30:55 -0400

    First card to load corresponds to user's region

--- dashboard.php
+++ dashboard.php
@@ -36,7 +36,7 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
  <body>
   <?php
 
-        //Get the connection info for the database
+      //Get the connection info for the database
       require_once 'includes/config.php';
 
       //Establish a database connection
@@ -52,9 +52,60 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
       if (isset($_POST['submit']) && $_POST['submit']=="LOG OUT") {
         unset($_SESSION['user']);
         $url = $_SERVER['REQUEST_URI'];
-        $newurl =  substr($url, 0, -9)."index.php";
+        $newurl =  substr($url, 0, -13)."index.php";
         echo '<script type="text/javascript">window.location ='."'".$newurl."'".'</script>';
       }
+
+      $login_email = $_SESSION['user'];
+      $user_query = "SELECT * FROM Users WHERE email = '$login_email'";
+
+      $user_result = $mysqli->query($user_query);
+      
+      if ( $user_result && $user_result->num_rows == 1) {
+
+          $row = $user_result->fetch_assoc();
+          $db_address = strtoupper($row["address"]);
+
+          //Load a specific card depending on what region they're from
+
+          $lansing_query = "SELECT * FROM Location WHERE region = 'Lansing'";
+          $lansing_result = $mysqli->query($lansing_query);
+          $lansing_addresses = array();
+          while ($lansing_row = $lansing_result->fetch_assoc()) {
+              array_push($lansing_addresses, $lansing_row["Street_Name"]);
+          }
+
+          $northside_query = "SELECT * FROM Location WHERE region = 'North'";
+          $northside_result = $mysqli->query($northside_query);
+          $northside_addresses = array();
+          while ($northside_row = $northside_result->fetch_assoc()) {
+              array_push($northside_addresses, $northside_row["Street_Name"]);
+          }
+
+          $southside_query = "SELECT * FROM Location WHERE region = 'South'";
+          $southside_result = $mysqli->query($southside_query);
+          $southside_addresses = array();
+          while ($southside_row = $southside_result->fetch_assoc()) {
+              array_push($southside_addresses, $southside_row["Street_Name"]);
+          }
+
+          $user_region = null;
+          foreach ($lansing_addresses as $address) {
+            if (strpos($db_address, $address) !== false) {
+              $user_region = 'sublansing';
+            }
+          }
+          foreach ($northside_addresses as $address) {
+            if (strpos($db_address, $address) !== false) {
+              $user_region = 'subnorth';
+            }
+          }
+          foreach ($southside_addresses as $address) {
+            if (strpos($db_address, $address) !== false) {
+              $user_region = 'subsouth';
+            }
+          }
+        }
 
     ?>
   <div class="clearfix borderbox" id="page"><!-- column -->
@@ -116,49 +167,67 @@ if(typeof Muse == "undefined") window.Muse = {}; window.Muse.assets = {"required
 	<div id = "next" onclick = "nextCard()" style = "position: absolute; top: 500; right: 50; z-index:100;">next</div-->
 	<table>
 		<tr>
-			<td id = "sublansing" class = "selected" onclick = "loadCard('sublansing')"></td>
-			<td id = "subnorth" class = "tmp" onclick = "loadCard('subnorth')"></td>
-			<td id = "subsouth" class = "tmp" onclick = "loadCard('subsouth')"></td>
+      <?php
+
+      if ($user_region == 'sublansing') {
+  			echo("<td id = 'sublansing' class = 'selected' onclick = loadCard('sublansing')></td>
+  			<td id = 'subnorth' class = 'tmp' onclick = loadCard('subnorth')></td>
+  			<td id = 'subsouth' class = 'tmp' onclick = loadCard('subsouth')></td>");
+      } else if ($user_region == 'subnorth') {
+        echo("<td id = 'sublansing' class = 'tmp' onclick = loadCard('sublansing')></td>
+        <td id = 'subnorth' class = 'selected' onclick = loadCard('subnorth')></td>
+        <td id = 'subsouth' class = 'tmp' onclick = loadCard('subsouth')></td>");
+      } else {
+        echo("<td id = 'sublansing' class = 'temp' onclick = loadCard('sublansing')></td>
+        <td id = 'subnorth' class = 'tmp' onclick = loadCard('subnorth')></td>
+        <td id = 'subsouth' class = 'selected' onclick = loadCard('subsouth')></td>");
+      }
+
+      ?>
 		</tr>
 	</table>
       <!--put the rest before div for vertical spacer-->
-    <script type = "text/javascript">
-		var selected = "sublansing";
+      <?php
+
+    echo("<script type = 'text/javascript'>
+		var selected = '$user_region';
 		$.ajax({
-        type: "GET",
-        url: "sublansing.php",
+        type: 'GET',
+        url: '$user_region.php',
         success: function (data) { 
-			$('#sublansing').html(data);
+			$('#$user_region').html(data);
         }
 		});
 		
 		function loadCard(region){
 			if (region == selected)
 				return;
-			$('#sublansing').html("");
-			$('#subnorth').html("");
-			$('#subsouth').html("");
+			$('#sublansing').html('');
+			$('#subnorth').html('');
+			$('#subsouth').html('');
 			$.ajax({
-			type: "GET",
-			url: region + ".php",
+			type: 'GET',
+			url: region + '.php',
 			success: function (data) { 
-			$('#sublansing').html("").removeClass();
-			$('#subnorth').html("").removeClass();
-			$('#subsouth').html("").removeClass();
+			$('#sublansing').html('').removeClass();
+			$('#subnorth').html('').removeClass();
+			$('#subsouth').html('').removeClass();
 			
-			$('#sublansing').html("").addClass("tmp");
-			$('#subnorth').html("").addClass("tmp");
-			$('#subsouth').html("").addClass("tmp");
+			$('#sublansing').html('').addClass('tmp');
+			$('#subnorth').html('').addClass('tmp');
+			$('#subsouth').html('').addClass('tmp');
 			
-			$("#"+region).removeClass();
-			$("#"+region).addClass("selected");
+			$('#'+region).removeClass();
+			$('#'+region).addClass('selected');
 			$('#'+region).html(data);
 			selected = region;
 			
 			}
 			
 			});
-		}
+		}")
+
+    ?>
 	</script>
     <div class="verticalspacer" data-offset-top="656" data-content-above-spacer="656" data-content-below-spacer="62"></div>
    </div>
